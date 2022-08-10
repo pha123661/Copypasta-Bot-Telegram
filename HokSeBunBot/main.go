689167d468c0aaf31c9b05f9707fd508997142e9
Li Sheng-Wei <pha123661@gmail.com>
2022-08-10 21:27:21 +0800

    Implement /new (新增複製文)

--- HokSeBunBot/main.go
+++ HokSeBunBot/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -21,15 +22,28 @@ func handleUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 			if _, err := bot.Send(replyMsg); err != nil {
 				fmt.Println(err)
 			}
-		default: // 複製文
-			command := update.Message.Command()
-			fmt.Println(path.Join(FILE_LOCATION, command+".txt"))
-			file, err := os.Create(path.Join(FILE_LOCATION, command+".txt"))
+		case "new": // 複製文
+			// write new file
+			split_tmp := strings.Split(update.Message.Text, " ")
+			if len(split_tmp) <= 2 {
+				return
+			}
+			fmt.Println(split_tmp)
+			var filename string = split_tmp[1] + ".txt"
+			fmt.Println(path.Join(FILE_LOCATION, filename))
+			file, err := os.Create(path.Join(FILE_LOCATION, filename))
 			if err != nil {
 				panic(err)
 			}
-			file.WriteString(update.Message.Text[len(command)+2:])
+			file.WriteString(update.Message.Text[len(update.Message.Command())+len(filename)-1:])
 			file.Close()
+
+			// send responce to user
+			replyMsg.Text = fmt.Sprintf("新增複製文「%s」成功", filename[:len(filename)-4])
+			replyMsg.ReplyToMessageID = update.Message.MessageID
+			if _, err := bot.Send(replyMsg); err != nil {
+				fmt.Println(err)
+			}
 		}
 
 	}
