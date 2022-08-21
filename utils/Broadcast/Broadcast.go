cf729f8a248e3433fad01bf508dd548e4ea8705c
Li Sheng-Wei <pha123661@gmail.com>
2022-08-21 20:34:09 +0800

    Update: add broadcast method

--- utils/Broadcast/Broadcast.go
+++ utils/Broadcast/Broadcast.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"go.mongodb.org/mongo-driver/bson"
@@ -37,6 +38,7 @@ func main() {
 		return
 	}
 	Text := string(B)
+	fmt.Println("已登入爲:", bot.Self.UserName)
 	fmt.Println("是否要傳送：")
 	fmt.Println("==============")
 	fmt.Println(Text)
@@ -46,26 +48,52 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	reader.ReadString('\n')
 
-	r, _ := regexp.Compile(`^-?[1-9]\d*`)
-	for _, ColName := range Collections {
-		switch ColName {
-		// case CONFIG.DB.CHAT_STATUS, CONFIG.DB.USER_STATUS, CONFIG.DB.GLOBAL_COL, "Beginner":
-		// 	continue
-		default:
+	fmt.Println("請問要送給\n[1] \\in DB\n[2] \\in UserStatus")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	switch input {
+	case "1":
+		r, _ := regexp.Compile(`^-?[1-9]\d*`)
+		for _, ColName := range Collections {
+			switch ColName {
+			// case CONFIG.DB.CHAT_STATUS, CONFIG.DB.USER_STATUS, CONFIG.DB.GLOBAL_COL, "Beginner":
+			// 	continue
+			default:
 
-			tmp := r.Find([]byte(ColName))
-			if tmp == nil {
-				continue
-			}
-			ChatID, _ := strconv.ParseInt(string(tmp), 10, 64)
-			Chat, err := bot.GetChat(tgbotapi.ChatInfoConfig{ChatConfig: tgbotapi.ChatConfig{ChatID: ChatID}})
-			if err != nil {
-				fmt.Println(ChatID, err)
-				continue
-			}
-			fmt.Printf("%+v\n", Chat)
+				tmp := r.Find([]byte(ColName))
+				if tmp == nil {
+					continue
+				}
+				ChatID, _ := strconv.ParseInt(string(tmp), 10, 64)
+				Chat, err := bot.GetChat(tgbotapi.ChatInfoConfig{ChatConfig: tgbotapi.ChatConfig{ChatID: ChatID}})
+				if err != nil {
+					fmt.Println(ChatID, err)
+					continue
+				}
+				fmt.Printf("%+v\n", Chat)
 
-			replyMsg := tgbotapi.NewMessage(ChatID, Text)
+				replyMsg := tgbotapi.NewMessage(ChatID, Text)
+				replyMsg.DisableNotification = true
+				replyMsg.ParseMode = "Markdown"
+				replyMsg.DisableWebPagePreview = true
+				_, err = bot.Send(replyMsg)
+				if err != nil {
+					fmt.Printf("[SendTR] ChatID: %d, Content:%s\n", ChatID, Text)
+					fmt.Println("[SendTR]", err)
+				}
+			}
+		}
+	case "2":
+		Curser, err := DB.Collection("0_UserStatus").Find(context.TODO(), bson.D{})
+		if err != nil {
+			panic(err)
+		}
+		for Curser.Next(context.TODO()) {
+			var v interface{}
+			Curser.Decode(&v)
+			ChatID := v.(bson.D)[1].Value
+			fmt.Println(ChatID)
+			replyMsg := tgbotapi.NewMessage(ChatID.(int64), Text)
 			replyMsg.DisableNotification = true
 			replyMsg.ParseMode = "Markdown"
 			replyMsg.DisableWebPagePreview = true
@@ -75,6 +103,9 @@ func main() {
 				fmt.Println("[SendTR]", err)
 			}
 		}
+	default:
+		fmt.Println("Get", input)
+		return
 	}
 	os.Rename("./WHATTOSAY.md", "./SENT.md")
 	os.Create("./WHATTOSAY.md")
